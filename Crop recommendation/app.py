fc5f714de8f745643dfd9f5f3aa005b23d9328ea
Mariastephan.J <63150376+Mariastephan@users.noreply.github.com>
2021-02-27 15:51:31 +0530

    Add files via upload

---
+++ Crop recommendation/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request
+#import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+import pandas as pd
+#from sklearn.preprocessing import MinMaxScaler
+app = Flask(__name__,template_folder='templates')
+with open('crop.pkl','rb') as f:
+        model=pickle.load(f)
+
+#standard_to = MinMaxScaler()
+@app.route("/", methods=['GET','POST'])
+def main():
+        #Fuel_Type_Diesel=0
+        if request.method == 'POST':
+                nitrogen=request.form.get('nitrogen')
+                phosphorus=request.form.get('phosphorus')
+                potassium=request.form.get('potassium')
+                temperature=request.form.get('temperature')
+                humidity=request.form.get('humidity')
+                ph=request.form.get('ph')
+                rainfall=request.form.get('rainfall')
+                crop=['rice', 'maize', 'kidney beans', 'black gram', 'pomegranate','banana','mango', 'grapes', 'apple', 'orange', 'papaya','coconut', 'cotton', 'jute', 'coffee']
+                prediction=model.predict([[nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall]])
+                #print(prediction)
+                # output=round(prediction[0],2)
+                # rec_crop=crop[prediction]
+                if prediction == 'rice':
+                        return render_template('main.html', result='The recommended crop is RICE')
+                if prediction == 'maize':
+                        return render_template('main.html', result='The recommended crop is MAIZE')
+                if prediction == 'kidney beans':
+                        return render_template('main.html', result='The recommended KIDNEY BEANS')
+                if prediction == 'black gram':
+                        return render_template('main.html', result='The recommended crop is BLACK GRAM')
+                if prediction == 'pomegranate':
+                        return render_template('main.html', result='The recommended crop is POMEGRANATE')
+                if prediction == 'banana':
+                        return render_template('main.html', result='The recommended crop is BANANA')
+                if prediction == 'mango':
+                        return render_template('main.html', result='The recommended crop is MANGO')
+                if prediction == 'grapes':
+                        return render_template('main.html', result='The recommended crop is GRAPES')
+                if prediction == 'apple':
+                        return render_template('main.html', result='The recommended crop is APPLE')
+                if prediction == 'orange':
+                        return render_template('main.html', result='The recommended crop is ORANGE')
+                if prediction == 'mouth beans':
+                        return render_template('main.html', result='The recommended crop is Moth Beans')
+                if prediction == 'mung bean':
+                        return render_template('main.html', result='The recommended crop is Mung Bean')
+                if prediction == 'peas':
+                        return render_template('main.html', result='The recommended crop is Peas')
+                if prediction == 'papaya':
+                        return render_template('main.html', result='The recommended crop is PAPAYA')
+                if prediction == 'coconut':
+                        return render_template('main.html', result='The recommended crop is COCONUT')
+                if prediction == 'cotton':
+                        return render_template('main.html', result='The recommended crop is COTTON')
+                if prediction == 'jute':
+                        return render_template('main.html', result='The recommended crop is JUTE')
+                if prediction == 'coffee':
+                        return render_template('main.html', result='The recommended crop is COFFEE')
+        else:
+                return render_template('main.html')
+if __name__=="__main__":
+        app.run(debug=True)
